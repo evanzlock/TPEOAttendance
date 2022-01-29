9e4b24f32d61ebe08297ee3397614a59f7689448
zshipchandler <91164602+zshipchandler@users.noreply.github.com>
2022-01-29 00:24:58 -0600

    add files

---
+++ services/z_absences.js
@@ -0,0 +1,39 @@
+const { Client } = require('@notionhq/client')
+const dotenv = require('dotenv').config()
+const notion = new Client({ auth: process.env.NOTION_TOKEN_ABSENCES, })
+
+const databaseMemberId = process.env.NOTION_DATABASE_MEMBER
+
+const databaseFormId = process.env.NOTION_DATABASE_FORM
+const databaseAbscencesId = process.env.NOTION_DATABASE_ABSENCES
+
+//variables that will be sent in from frontend (hardcoded for now)
+var meetingNumber = 6;
+var meetingType = "Product";
+
+const notionAbsences = new Client({
+    auth: process.env.NOTION_TOKEN_ABSENCES,
+  })
+
+
+
+module.exports = async function getAbsenceForms() {
+    const payload = {
+        path: `databases/${databaseAbscencesId}/query`,
+        method: 'POST'
+    }
+    const {results} = await notionAbsences.request(payload);
+    
+    const absences = results.map(page => {
+        if (page.properties['Meeting Number'].number === meetingNumber 
+            && page.properties['Type of Meeting'].multi_select[0].name === meetingType) {
+                return {
+                    name: page.properties.Name.title[0].text.content,
+                    meetingN: page.properties['Meeting Number'].number,
+                    meetingType: page.properties['Type of Meeting'].multi_select[0].name
+                }
+
+            }
+    })
+    return absences;
+}
---
+++ services/z_forms.js
@@ -0,0 +1,35 @@
+const { Client } = require('@notionhq/client')
+const dotenv = require('dotenv').config()
+const notion = new Client({ auth: process.env.NOTION_KEY, })
+
+const databaseMemberId = process.env.NOTION_DATABASE_MEMBER
+
+const databaseFormId = process.env.NOTION_DATABASE_FORM
+
+//variables that will be sent in from frontend (hardcoded for now)
+var meetingNumber = 1;
+var meetingType = "General";
+var password = "first";
+
+
+module.exports = async function getForms() {
+    const payload = {
+        path: `databases/${databaseFormId}/query`,
+        method: 'POST'
+    }
+    const { results } = await notion.request(payload)
+    console.log(results)
+    const forms = results.map(page => {
+        console.log(page.properties.Name.title);
+        if (page.properties['Meeting Number'].number === meetingNumber 
+            && page.properties['Type of Meeting'].multi_select[0].name === meetingType
+            && page.properties['Code Submitted'].rich_text[0].text.content === password) {
+                return {
+                    name: page.properties.Name.title[0].text.content,
+                    code: page.properties['Code Submitted'].rich_text[0].text.content
+                }
+            }
+ 
+    })
+    return forms
+}
