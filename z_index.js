8908326beff3e8aaae8515d8439053b42f2eb519
zshipchandler <91164602+zshipchandler@users.noreply.github.com>
2022-01-29 11:09:43 -0600

    new changes

---
+++ z_index.js
@@ -0,0 +1,113 @@
+const getMembers = require('./services/z_notion')
+const getForms = require('./services/z_forms')
+const getAbsenceForms = require('./services/z_absences');
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+
+const app = express()
+
+//middleware
+app.use(express.static('public'))
+
+app.get('/members', async (req, res) => {
+    const members = await getMembers()
+    res.json(members)
+})
+app.get('/forms', async (req, res) => {
+    const forms = await getForms()
+    res.json(forms)
+})
+app.get('/absences', async (req,res) => {
+    const absences = await getAbsenceForms();
+    res.json(absences);
+})
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
+
+//variables that will be sent in from frontend (hardcoded for now)
+var meetingType = "Engineering";
+
+const updateMembersData = async () => {
+    const { Client } = require('@notionhq/client');
+    const notion = new Client({ auth: process.env.NOTION_KEY});
+
+    //TODO: MAKE MORE EFFICIENT- should only do this computation 
+    //once and figure out how to store it
+    const absences = await getAbsenceForms();
+    const present = await getForms();
+
+    constAbsencesSet = new Set();
+    for (var i = 0; i < absences.length; i++) {
+        var obj = absences[i];
+        if (obj != undefined) {
+            constAbsencesSet.add(obj.name);
+        }
+    }
+    console.log(constAbsencesSet);
+    constPresentSet = new Set();
+    for (var i = 0; i < present.length; i++) {
+        var obj = present[i];
+        if (obj != undefined) {
+            constPresentSet.add(obj.name);
+        }
+    }
+    console.log(constPresentSet);
+
+    const members = await getMembers();
+    for (var i = 0; i < members.length; i++) {
+        var obj = members[i];
+        console.log(obj);
+        if (obj != undefined) {
+            //ensures only general and team-specific meetings counted for attendance
+            if (meetingType === "General" || obj.team === meetingType) {
+                if (constPresentSet.has(obj.name)) {
+                    //console.log("present " + obj.name);
+                    //function that updates members data base with 1 more meeting attended
+                    (async () => {
+                        const pageId = obj.pageid;
+                        const response = await notion.pages.update({
+                          page_id: pageId,
+                          properties: {
+                            "Total Meetings Attended": obj.total + 1
+                          },
+                        });
+                        console.log(response);
+                      })();
+                } 
+                else if (constAbsencesSet.has(obj.name)) {
+                    //console.log("excused " + obj.name);
+                    //function that updates members data base with 1 more excused absence
+                    (async () => {
+                        const pageId = obj.pageid;
+                        const response = await notion.pages.update({
+                          page_id: pageId,
+                          properties: {
+                            "Excused Absences": obj.excused + 1
+                          },
+                        });
+                        console.log(response);
+                      })();
+                } else {
+                    //console.log("unexcused " + obj.name);
+                    //function that updates members data base with 1 more unexcused absence
+                    (async () => {
+                        const pageId = obj.pageid;
+                        const response = await notion.pages.update({
+                          page_id: pageId,
+                          properties: {
+                            "Unexcused Absences": obj.unexcused + 1
+                          },
+                        });
+                        console.log(response);
+                      })();
+                }
+            }
+            
+            
+        }
+    }
+
+}
+
+updateMembersData();
