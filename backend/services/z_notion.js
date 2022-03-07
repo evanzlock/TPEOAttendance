fd19029b58ba7f0fcff8c88829aa87160da2f6c8
Evan <lock.evan@gmail.com>
2022-03-07 10:43:47 -0600

    delete unnecessary code

--- backend/services/z_notion.js
+++ backend/services/z_notion.js
@@ -1,34 +1,7 @@
 const dotenv = require('dotenv').config();
 const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: process.env.NOTION_KEY, });
-//console.log(process.env.NOTION_KEY);
-//const credentials = require("./cred.json");
-const cors = require("cors");
-//database variables
-const databaseMemberId = process.env.NOTION_DATABASE_MEMBER
-const databaseFormId = process.env.NOTION_DATABASE_FORM
 
-//setting up express server
-const express = require('express');
-
-const app = express();
-
-
-//app.listen(process.env.PORT || 3000, console.log('Server is up and running!') );
-// app.get("/", (req, res) => {
-//     console.log("");
-// });
-// //change password status
-// app.put("/code", (req, res) => {
-//     const body = req.body;
-//     password.status = !password.status;
-//     password.code = req.body.code;
-//     if(password.status)
-//     {
-//         return res.json({ msg: "Code generated", data: {} });
-//     }
-//     return res.json({msg: "Code disabled", data: {}});
-// });
 module.exports = async function getForms() {
     const payload = {
         path: `databases/${databaseFormId}/query`,
