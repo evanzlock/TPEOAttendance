c38e37ef52112a0c22638f1737ce9bc6a50f268a
Zara Shipchandler <zarashipchandler@Zaras-MacBook-Air.local>
2022-03-14 16:39:49 -0500

    updates meetingHistory in updateMembersData()

---
+++ backend/services/meetingHistory.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv').config();
+const { Client } = require('@notionhq/client');
+const notion = new Client({ auth: process.env.NOTION_MEETINGHISTORY_TOKEN })
+
+//database variables
+const databaseId = process.env.NOTION_MEETINGHISTORY_ID
+
+
+//setting up express server
+const express = require('express');
+
+const app = express( );
+
+module.exports = async function getMeetingHistory() {
+    const payload = {
+        path: `databases/${databaseId}/query`,
+        method: 'POST'
+    }
+    const { results } = await notion.request(payload)
+
+    const history = results.map(page => {
+        return {
+            meetingType: page.properties.MeetingType.title[0].text.content,
+            meetingNumber: page.properties['Meeting #'].number,
+            numAttended: page.properties['Attended'].number,
+            numAttended: page.properties['# Unexcused Absences'].number,
+            attendancePercent: page.properties['%'].formula.number
+        }
+    })
+    return history
+}
+
+
+
+
+
+
--- backend/z_index.js
+++ backend/z_index.js
@@ -1,6 +1,7 @@
 const getMembers = require('./services/z_notion')
 const getForms = require('./services/z_forms')
 const getAbsenceForms = require('./services/z_absences');
+const getMeetingHistory = require('./services/meetingHistory');
 const express = require('express')
 const PORT = process.env.PORT || 5000
 const router = express.Router();
@@ -36,6 +37,10 @@ app.get('/absences', async (req, res) => {
   const absences = await getAbsenceForms();
   res.json(absences);
 })
+app.get('/meetingHistory', async (req,res) => {
+  const meetingHist = await getMeetingHistory();
+  res.json(meetingHist);
+})
 app.post('/meeting', (req, res) => {
   if (req.body == undefined) {
     return res.json({
@@ -67,44 +72,45 @@ app.listen(PORT, console.log(`Server started on port ${PORT}`))
 
 //variables that will be sent in from frontend (hardcoded for now)
 var meetingType = "Engineering";
+var meetingNumber= 1;
 
 const updateMembersData = async () => {
   const { Client } = require('@notionhq/client');
   const notion = new Client({ auth: process.env.NOTION_KEY });
+  var unexcused = 0;
+  var attended = 0;
 
-  //TODO: MAKE MORE EFFICIENT- should only do this computation 
-  //once and figure out how to store it
   const absences = await getAbsenceForms();
   const present = await getForms();
 
   constAbsencesSet = new Set();
-  for (var i = 0; i < absences.length; i++) {
-    var obj = absences[i];
-    if (obj != undefined) {
-      constAbsencesSet.add(obj.name);
+  if (absences) {
+    for (var i = 0; i < absences.length; i++) {
+      var obj = absences[i];
+      if (obj != undefined) {
+        constAbsencesSet.add(obj.name);
+      }
     }
   }
-  //console.log(constAbsencesSet);
   constPresentSet = new Set();
-  for (var i = 0; i < present.length; i++) {
-    var obj = present[i];
-    if (obj != undefined) {
-      constPresentSet.add(obj.name);
+  if (present) {
+    for (var i = 0; i < present.length; i++) {
+      var obj = present[i];
+      if (obj != undefined) {
+        constPresentSet.add(obj.name);
+        attended++;
+      }
     }
   }
-  //console.log(constPresentSet);
 
   const members = await getMembers();
   for (var i = 0; i < members.length; i++) {
     var obj = members[i];
-    //console.log(obj);
     if (obj != undefined) {
       //ensures only general and team-specific meetings counted for attendance
       if (meetingType === "General" || obj.team === meetingType) {
         if (constPresentSet.has(obj.name)) {
-          //console.log("present " + obj.name);
           //function that updates members data base with 1 more meeting attended
-          (async () => {
             const pageId = obj.pageid;
             const response = await notion.pages.update({
               page_id: pageId,
@@ -112,13 +118,10 @@ const updateMembersData = async () => {
                 "Total Meetings Attended": obj.total + 1
               },
             });
-            //console.log(response);
-          })();
         }
         else if (constAbsencesSet.has(obj.name)) {
           //console.log("excused " + obj.name);
           //function that updates members data base with 1 more excused absence
-          (async () => {
             const pageId = obj.pageid;
             const response = await notion.pages.update({
               page_id: pageId,
@@ -126,12 +129,8 @@ const updateMembersData = async () => {
                 "Excused Absences": obj.excused + 1
               },
             });
-            //console.log(response);
-          })();
         } else {
-          //console.log("unexcused " + obj.name);
           //function that updates members data base with 1 more unexcused absence
-          (async () => {
             const pageId = obj.pageid;
             const response = await notion.pages.update({
               page_id: pageId,
@@ -139,15 +138,24 @@ const updateMembersData = async () => {
                 "Unexcused Absences": obj.unexcused + 1
               },
             });
-            //console.log(response);
-          })();
+          unexcused++;
         }
       }
-
-
     }
   }
+  //add this meeting info to Meeting History Database
+  const notionMeeting = new Client({ auth: process.env.NOTION_MEETINGHISTORY_TOKEN});
+  notionMeeting.pages.create({
+    parent: {database_id: process.env.NOTION_MEETINGHISTORY_ID},
+    properties: {
+      "title": [{"text": {"content": meetingType}}],
+      "Meeting #": meetingNumber,
+      "Attended": attended,
+      "# Unexcused Absences": unexcused,
+    }
+  });
+  return {type: meetingType, unexcused: unexcused, attended: attended};
 
 }
 //TODO: call updateMembersData() when meeting timer ends on frontend
-updateMembersData();
+//updateMembersData();
