5d6ce10bd0b78b867c707b50e78cb5072c0d87f6
Evan <lock.evan@gmail.com>
2022-03-16 02:57:02 -0500

    add timer

---
+++ frontend/my-app/src/components/Timer.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import '../style.css';
+export default function Timer(props) {
+
+    const [time, setTime] = useState({
+        second: '00',
+        minute: String(props.minutes),
+        counter: props.minutes * 60
+    })
+    const [isActive, setIsActive] = useState(false);
+    useEffect(() => {
+        let intervalId;
+        if (isActive && time.counter !== -1) {
+            intervalId = setInterval(() => {
+                const secondCounter = time.counter % 60;
+                const minuteCounter = Math.floor(time.counter / 60);
+
+                const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}` : secondCounter;
+                const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}` : minuteCounter;
+
+                setTime(prevState => ({
+                    second: computedSecond,
+                    minute: computedMinute,
+                    counter: prevState.counter - 1
+                }))
+            }, 1000)
+        }
+        return () => clearInterval(intervalId);
+    }, [isActive, time.counter])
+    return (
+        <div className="container">
+            <div className="time">
+                <span className="minute">{time.minute}</span>
+                <span>:</span>
+                <span className="second">{time.second}</span>
+            </div>
+            <div className="buttons">
+                <button onClick={() => setIsActive(!isActive)} className="start">
+                    {isActive ? "Pause" : "Start"}
+                </button>
+            </div>
+        </div>
+    )
+}
+
