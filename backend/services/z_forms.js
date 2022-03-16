2f5276b092c73cfedcbc773b6dbd1ce84fa1123d
Zara Shipchandler <zarashipchandler@Zaras-MacBook-Air.local>
2022-03-15 20:32:48 -0500

    comments on logic

--- backend/services/z_forms.js
+++ backend/services/z_forms.js
@@ -20,7 +20,8 @@ module.exports = async function getForms() {
     const { results } = await notion.request(payload)
     //console.log(results)
     const forms = results.map(page => {
-        //console.log(page.properties.Name.title);
+        //parses through the form records
+        //only returns the forms that have the correct meeting type/number and code
         if (page.properties['Meeting Number'].number === meetingNumber
             && page.properties['Type of Meeting'].multi_select[0].name === meetingType
             && page.properties['Code Submitted'].rich_text[0].text.content === password) {
