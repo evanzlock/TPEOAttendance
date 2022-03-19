3d7f2454a1ac87f20974f12c7a6c6c5174de5319
Evan <lock.evan@gmail.com>
2022-03-19 05:34:13 -0500

    resolve merge conflicts

--- backend/services/z_absences.js
+++ backend/services/z_absences.js
@@ -8,23 +8,13 @@ const databaseFormId = process.env.NOTION_DATABASE_FORM
 const databaseAbscencesId = process.env.NOTION_DATABASE_ABSENCES
 
 //variables that will be sent in from frontend (hardcoded for now)
-<<<<<<< HEAD
-=======
-var meetingNumber = 1;
-var meetingType = "Engineering";
-
->>>>>>> e6cb0a28f6f775d09ad5bbc11a282585fecb214c
 const notionAbsences = new Client({
     auth: process.env.NOTION_TOKEN_ABSENCES,
 })
 
 
 
-<<<<<<< HEAD
 module.exports = async function getAbsenceForms(meetingNumber, meetingType) {
-=======
-module.exports = async function getAbsenceForms(meetingNumber, meetingType, password) {
->>>>>>> e6cb0a28f6f775d09ad5bbc11a282585fecb214c
     const payload = {
         path: `databases/${databaseAbscencesId}/query`,
         method: 'POST'
