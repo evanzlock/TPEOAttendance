ddba95ae3785c6d6ef8a34fbd797c95ea1ba54fa
Evan <lock.evan@gmail.com>
2022-03-19 05:29:14 -0500

    adjust syntax

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 import CreateMeeting from './components/CreateMeeting';
-<<<<<<< HEAD
 import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <div className="App">
       <Dashboard></Dashboard>
-=======
-function App() {
-  return (
-    <div className="App">
-      <CreateMeeting></CreateMeeting>
->>>>>>> e6cb0a28f6f775d09ad5bbc11a282585fecb214c
     </div>
   );
 }
